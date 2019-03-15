784b712595ae9226c41154e16cb7aa14f9ad6aac
Eric <keric.yi@gmail.com>
2019-03-15 00:04:54 -0700

    italian menu items

---
+++ src/Components/MenuComponent.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import { Media } from 'reactstrap';
+import './MenuComponent.css';
+
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: [
+                {
+                    id: 0,
+                    name: 'Meat and Cheese Platter',
+                    image: 'assets/images/meat_cheese_platter.jpg',
+                    category: 'appetizer',
+                    label: '',
+                    price: '4.99',
+                    description:'A collection of specially selected meats and cheeses paired with freshly baked breadsticks and grapes.'
+                },
+                {
+                    id: 1,
+                    name: 'Chicken Fettucine Alfredo',
+                    image: 'assets/images/chicken_fettucine_alfredo.jpg',
+                    category: 'mains',
+                    label: 'Hot',
+                    price: '11.99',
+                    description: 'Sliced grilled chicken and homemade alfredo sauce over fettuccine pasta.'
+                },
+                {
+                    id: 2,
+                    name: 'Meat Lover\'s Lasagna',
+                    image: 'assets/images/meat_lasagna.jpg',
+                    category: 'mains',
+                    label: 'New',
+                    price: '12.99',
+                    description: 'Layered lasgna noodles, italian sausage and beef, ricotta cheese, tomato sauce, and baked mozzarella and romano cheese.'
+                },
+                {
+                    id: 3,
+                    name: 'Tomato Basil Penne Pasta',
+                    image: 'assets/images/tomato_basil_penne.jpg',
+                    category: 'mains',
+                    label: '',
+                    price: '10.99',
+                    description: 'Penne pasta combined with three differnt cheeses and topped with a spicy marinara sauce.'
+                },
+                {
+                    id: 4,
+                    name: '4 Topping Pizza',
+                    image: 'assets/images/pizza.jpg',
+                    category: 'mains',
+                    label: '',
+                    price: '14.99',
+                    description: 'A delicious blend of three different cheese, tomato basil sauce, and your choice of four other ingredients, all baked together into one pizza. (Choose 4 - Pepperoni, Sausage, Ham, Olives, Onions, Bell Peppers, Mushrooms, Extra Cheese)'
+                },
+                {
+                    id: 5,
+                    name: 'Italian Gelato',
+                    image: 'assets/images/gelato.jpg',
+                    category: 'desserts',
+                    label: '',
+                    price: '4.99',
+                    description: 'Three scoops of delectable, creamy Italian ice cream, paired with a waffle cone. (Strawberry, Vanilla, Chocolate, Hazelnut, Coffee)'
+                }
+            ],
+        };
+    }
+
+    render() {
+        const menu = this.state.dishes.map((dish) => {
+            return (
+                <div key={dish.id} className="col-12 mt-5">
+                    <Media tag="li">
+                        <Media left middle>
+                            <Media object src={dish.image} alt={dish.name} className='menuImage' />
+                        </Media>
+                        <Media body className="ml-5">
+                            <Media heading>{dish.name}</Media>
+                            <p>{dish.description}</p>
+                        </Media>
+                    </Media>
+                </div>
+            );
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <Media list>
+                        {menu}
+                    </Media>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Menu;
